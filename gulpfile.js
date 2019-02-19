2b88a8abf159739dfcc1bd23d976a4454357ebb9
Edvard Chumakov <zerosouler@gmail.com>
2019-02-19 18:43:08 +0300

    Gulp-4 build v1.0

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp'),
+glp = require('gulp-load-plugins'),
+browserSync = require('browser-sync').create();
+plugins = glp();
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		}
+	})
+});
+
+gulp.task('stylus', function(done) {
+	return gulp.src('app/css/main.css')
+	.pipe(plugins.stylus({
+		'include css': true
+	}))
+	.pipe(plugins.autoprefixer({
+		browsers: ['last 4 versions'],
+            cascade: false
+	}))
+	.pipe(plugins.concat('main.css'))
+	.pipe(plugins.csso({ 
+		restructure: false,
+		sourceMap: true,
+		debug: true
+	}))
+	.pipe(gulp.dest('dist/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+	done()
+});
+
+gulp.task('scripts', function(done) {
+	return gulp.src(['app/js/**/*.js', 'app/js/main.js'])
+	.pipe(plugins.concat('main.js'))
+	.pipe(plugins.uglify())
+	.pipe(gulp.dest('dist/js'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+	done()
+});
+
+gulp.task('img', function(done) {
+	return gulp.src('app/img/**/*.*')
+	.pipe(plugins.image())
+	.pipe(gulp.dest('dist/img'))
+	done()
+});
+
+gulp.task('indexmin', function(done) {
+	return gulp.src('app/*.html')
+	.pipe(plugins.htmlmin({ collapseWhitespace: true }))
+	.pipe(gulp.dest('dist'))
+	done()
+});
+
+gulp.task('default', gulp.series(
+	gulp.parallel('serve', 'watch')
+));
+	
+gulp.task('fonts', function(done) {
+	return gulp.src('app/font/*.*')
+		.pipe(gulp.dest('dist/font'))
+		done()
+});
+	
+gulp.task('watch', function() {
+	gulp.watch('app/**/*.styl', gulp.series('stylus'))
+	gulp.watch('app/**/*.css', gulp.series('stylus'))
+	gulp.watch('app/js/**/*.js', gulp.series('scripts'))			
+	gulp.watch('app/index.html').on('change', browserSync.reload)										  	 
+});
+	
+gulp.task('build', gulp.parallel('scripts','fonts','stylus','indexmin'));
