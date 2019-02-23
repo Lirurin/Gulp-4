bb64da7b592b397948a223ce922dfdf3beb08d93
Edvard <43088094+Lirurin@users.noreply.github.com>
2019-02-23 19:31:27 +0300

    Fixes, build 1.05

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ plugins = glp();
 gulp.task('serve', function() {
 	browserSync.init({
 		server: {
-			baseDir: 'app/'
+			baseDir: 'dist/'
 		}
 	})
 });
 
 gulp.task('stylus', function(done) {
-	return gulp.src('app/css/main.css')
+	return gulp.src('app/css/*.{css,styl}')
 	.pipe(plugins.stylus({
 		'include css': true
 	}))
@@ -58,21 +58,24 @@ gulp.task('indexmin', function(done) {
 	done()
 });
 
-gulp.task('default', gulp.series(
-	gulp.parallel('serve', 'watch')
-));
-	
+
 gulp.task('fonts', function(done) {
 	return gulp.src('app/font/*.*')
-		.pipe(gulp.dest('dist/font'))
-		done()
+	.pipe(gulp.dest('dist/font'))
+	done()
 });
-	
+
 gulp.task('watch', function() {
 	gulp.watch('app/**/*.styl', gulp.series('stylus'))
 	gulp.watch('app/**/*.css', gulp.series('stylus'))
-	gulp.watch('app/js/**/*.js', gulp.series('scripts'))			
-	gulp.watch('app/index.html').on('change', browserSync.reload)										  	 
+	gulp.watch('app/js/**/*.js', gulp.series('scripts'))		
+	gulp.watch('app/font/**/*.*', gulp.series('fonts'))		
+	gulp.watch('app/index.html', gulp.series('indexmin')).on('change', browserSync.reload)										  	 
 });
-	
+
+gulp.task('default', gulp.series(
+	gulp.parallel('stylus', 'scripts','indexmin','fonts'), 
+	gulp.parallel('serve', 'watch')
+));
+
 gulp.task('build', gulp.parallel('scripts','fonts','stylus','indexmin'));
