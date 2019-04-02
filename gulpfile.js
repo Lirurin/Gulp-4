e9e2e088ba5a5113ba1a9b254f53980989bd3fc1
Edvard <43088094+Lirurin@users.noreply.github.com>
2019-04-02 20:19:48 +0300

    Pug, build 1.07

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
 glp = require('gulp-load-plugins'),
+pug = require('gulp-pug'),
 browserSync = require('browser-sync').create();
 plugins = glp();
 
+//localhost
 gulp.task('serve', function() {
 	browserSync.init({
 		server: {
@@ -10,7 +12,19 @@ gulp.task('serve', function() {
 		}
 	})
 });
-
+//PUG
+gulp.task('pug', function (done) {
+	return gulp.src('app/*.pug')
+	.pipe(pug({
+		pretty: true
+	}))
+	.pipe(gulp.dest('dist'))						
+	.pipe(browserSync.reload({
+		stream: true							
+	}));
+	done()
+});
+//stylus
 gulp.task('stylus', function(done) {
 	return gulp.src('app/css/*.{css,styl}')
 	.pipe(plugins.stylus({
@@ -32,7 +46,7 @@ gulp.task('stylus', function(done) {
 	}))
 	done()
 });
-
+//JS
 gulp.task('scripts', function(done) {
 	return gulp.src(['app/js/**/*.js', 'app/js/main.js'])
 	.pipe(plugins.concat('main.js'))
@@ -43,38 +57,38 @@ gulp.task('scripts', function(done) {
 	}))
 	done()
 });
-
+//Img min
 gulp.task('img', function(done) {
 	return gulp.src('app/img/**/*.*')
 	.pipe(plugins.image())
 	.pipe(gulp.dest('dist/img'))
 	done()
 });
-
+//html min
 gulp.task('indexmin', function(done) {
 	return gulp.src('app/*.html')
 	.pipe(plugins.htmlmin({ collapseWhitespace: true }))
 	.pipe(gulp.dest('dist'))
 	done()
 });
-
-
+//font relocation
 gulp.task('fonts', function(done) {
 	return gulp.src('app/font/*.*')
 	.pipe(gulp.dest('dist/font'))
 	done()
 });
-
+//watch
 gulp.task('watch', function() {
 	gulp.watch('app/**/*.styl', gulp.series('stylus'))
 	gulp.watch('app/**/*.css', gulp.series('stylus'))
 	gulp.watch('app/js/**/*.js', gulp.series('scripts'))		
 	gulp.watch('app/font/**/*.*', gulp.series('fonts'))		
-	gulp.watch('app/index.html', gulp.series('indexmin')).on('change', browserSync.reload)										  	 
+	gulp.watch('app/*.pug', gulp.series('pug'))
+	gulp.watch('app/*.html', gulp.series('indexmin')).on('change', browserSync.reload)										  	 
 });
-
+//user tasks
 gulp.task('default', gulp.series(
-	gulp.parallel('stylus', 'scripts','indexmin','fonts'), 
+	gulp.parallel('stylus', 'scripts','indexmin','fonts', 'pug'), 
 	gulp.parallel('serve', 'watch')
 ));
 
