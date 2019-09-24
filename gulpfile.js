35a9c9e281fd0e68908474976df68d04c24d0a84
Edvard <43088094+Lirurin@users.noreply.github.com>
2019-09-24 13:37:30 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,150 @@
 var gulp = require('gulp'),
-glp = require('gulp-load-plugins'),
-pug = require('gulp-pug'),
-babel = require('gulp-babel'),
-browserSync = require('browser-sync').create();
-plugins = glp();
+	glp = require('gulp-load-plugins')(),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	browserSync = require('browser-sync').create();
 
-//localhost
-gulp.task('serve', function() {
-	browserSync.init({
-		server: {
-			baseDir: 'dist/'
-		}
-	})
-});
-//PUG
-gulp.task('pug', function (done) {
-	return gulp.src('app/*.pug')
-	.pipe(pug({
-		pretty: true
+gulp.task('pug', function() {
+	return gulp.src('app/pug/*.pug')
+	.pipe(glp.pug({
+		pretty: true 
 	}))
-	.pipe(gulp.dest('dist'))						
-	.pipe(browserSync.reload({
-		stream: true							
-	}));
-	done()
+	.pipe(gulp.dest('dist/'))
+	.on('end', browserSync.reload)
 });
-//stylus
-gulp.task('stylus', function(done) {
-	return gulp.src('app/css/*.{css,styl}')
-	.pipe(plugins.stylus({
+
+gulp.task('stylus:conc', function() {
+	return gulp.src('app/stylus/*.styl')
+	.pipe(glp.stylus({
 		'include css': true
 	}))
-	.pipe(plugins.autoprefixer({
-		browsers: ['last 4 versions'],
-            cascade: false
+	.pipe(glp.autoprefixer({
+		browsers: ['ie >= 8', 'last 4 version']
 	}))
-	.pipe(plugins.concat('main.css'))
-	.pipe(plugins.csso({ 
-		restructure: false,
-		sourceMap: true,
-		debug: true
-	}))
+	.pipe(glp.concat('main.css'))
+	// .pipe(glp.csso())
 	.pipe(gulp.dest('dist/css'))
 	.pipe(browserSync.reload({
 		stream: true
 	}))
-	done()
 });
-//JS
-gulp.task('scripts', function(done) {
-	return gulp.src(['app/js/**/*.js', 'app/js/main.js'])
-	.pipe(babel({
-		presets: ['@babel/env']
+gulp.task('stylus:no-conc', function() {
+	return gulp.src('app/stylus/noconcat/*.styl')
+	.pipe(glp.stylus({
+		'include css': true
 	}))
-	.pipe(plugins.concat('main.js'))
-	.pipe(plugins.uglify())
+	.pipe(glp.autoprefixer({
+		browsers: ['ie >= 8', 'last 4 version']
+	}))
+	// .pipe(glp.csso())
+	.pipe(gulp.dest('dist/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('app/js/*.js')
+	// .pipe(glp.browserify({
+	// 	insertGlobals : true
+	// }))
+	.pipe(glp.babel({
+		"presets": ["@babel/preset-env"]
+	}))
+	// .pipe(glp.uglify())
 	.pipe(gulp.dest('dist/js'))
 	.pipe(browserSync.reload({
 		stream: true
 	}))
-	done()
 });
-//Img min
-gulp.task('img', function(done) {
+
+gulp.task('scripts:libs', function() {
+	return gulp.src('app/js/libs/*.js')
+	.pipe(glp.uglify())
+	.pipe(gulp.dest('dist/js/libs'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+gulp.task('scripts:scripts', function() {
+	return gulp.src('app/js/scripts/*.js')
+	.pipe(glp.uglify())
+	.pipe(gulp.dest('dist/js/scripts'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('static', function() {
+	return gulp.src('app/static/**/*.*')
+	.pipe(gulp.dest('dist/'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+gulp.task('fonts', function() {
+	return gulp.src('app/fonts/**/*.*')
+	.pipe(gulp.dest('dist/css/fonts/'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('img:dev', function() {
 	return gulp.src('app/img/**/*.*')
-	.pipe(plugins.image())
 	.pipe(gulp.dest('dist/img'))
-	done()
+	.pipe(browserSync.reload({
+		stream: true
+	}))
 });
-//html min
-gulp.task('indexmin', function(done) {
-	return gulp.src('app/*.html')
-	.pipe(plugins.htmlmin({ collapseWhitespace: true }))
-	.pipe(gulp.dest('dist'))
-	done()
+
+gulp.task('img:build', function() {
+	return gulp.src('app/img/**/*.*')
+	.pipe(imagemin([
+		imagemin.gifsicle({interlaced: true}),
+		imagemin.jpegtran({progressive: true}),
+		imagemin.optipng({optimizationLevel: 5}),
+		imagemin.svgo({
+			plugins: [
+				{removeViewBox: true},
+				{cleanupIDs: false}
+			]
+		})
+	]))
+	.pipe(gulp.dest('dist/img'))
 });
-//font relocation
-gulp.task('fonts', function(done) {
-	return gulp.src('app/font/*.*')
-	.pipe(gulp.dest('dist/font'))
-	done()
+
+gulp.task('clean', function() {
+	return del('dist');
 });
-//watch
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			injectChanges: true,
+			baseDir: 'dist/'
+		}
+	})
+});
+
 gulp.task('watch', function() {
-	gulp.watch('app/**/*.styl', gulp.series('stylus'))
-	gulp.watch('app/**/*.css', gulp.series('stylus'))
-	gulp.watch('app/js/**/*.js', gulp.series('scripts'))		
-	gulp.watch('app/font/**/*.*', gulp.series('fonts'))		
-	gulp.watch('app/*.pug', gulp.series('pug'))	
-	gulp.watch('app/*.html', gulp.series('indexmin')).on('change', browserSync.reload)										  	 
+	gulp.watch('app/**/**/*.pug', gulp.series('pug'))
+	gulp.watch('app/**/**/*.styl', gulp.series('stylus:no-conc'))
+	gulp.watch('app/**/**/*.css', gulp.series('stylus:no-conc'))
+	gulp.watch('app/**/**/*.styl', gulp.series('stylus:conc'))
+	gulp.watch('app/**/**/*.css', gulp.series('stylus:conc'))
+	gulp.watch('app/**/**/*.js', gulp.series('scripts'))
+	gulp.watch('app/js/libs/*.js', gulp.series('scripts:libs'))
+	gulp.watch('app/js/scripts/*.js', gulp.series('scripts:scripts'))
+	gulp.watch('app/static/**/*.*', gulp.series('static'))
+	gulp.watch('app/img/**/*.png', gulp.series('img:dev'))
 });
-//user tasks
+
 gulp.task('default', gulp.series(
-	gulp.parallel('stylus', 'scripts','indexmin','fonts', 'pug'), 
+	gulp.parallel('pug', 'stylus:conc', 'stylus:no-conc', 'scripts:libs', 'scripts:scripts', 'scripts', 'fonts', 'static', 'img:dev'), 
 	gulp.parallel('serve', 'watch')
 ));
 
-gulp.task('build', gulp.parallel('scripts','fonts','stylus','indexmin'));
+gulp.task('build', gulp.series(
+	'clean', 'pug', 'stylus:conc', 'stylus:no-conc', 'scripts', 'scripts:libs', 'scripts:scripts','fonts', 'static', 'img:build'
+));
